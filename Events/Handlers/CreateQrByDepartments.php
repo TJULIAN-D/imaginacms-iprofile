6b86b5c47750d64497fc972848176c6f73a54253
Cristian Marin <marin101998@gmail.com>
2022-06-25 14:38:39 -0500

    + add |  handler qr and vcard

---
+++ Events/Handlers/CreateQrByDepartments.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Modules\Iprofile\Events\Handlers;
+
+use Exception;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Lcobucci\JWT\Parser;
+use Modules\Core\Http\Controllers\BasePublicController;
+use Modules\Ihelpers\Http\Controllers\Api\BaseApiController;
+use Modules\User\Entities\Sentinel\User;
+use Modules\Iprofile\Events\ImpersonateEvent;
+use Modules\Iprofile\Repositories\UserApiRepository;
+use Modules\User\Exceptions\InvalidOrExpiredResetCode;
+use Modules\User\Exceptions\UserNotFoundException;
+use Modules\User\Http\Requests\ResetCompleteRequest;
+use Modules\User\Http\Requests\ResetRequest;
+use Modules\User\Services\UserResetter;
+use Socialite;
+use Modules\User\Contracts\Authentication;
+
+class CreateQrByDepartments
+{
+
+  public $qrService;
+
+  public function __construct()
+  {
+    $this->qrService = app("Modules\Qreable\Services\QrService");
+  }
+
+  public function handle($event)
+  {
+    if(is_module_enabled('Qreable')){
+      try {
+        $user = $event->user;
+        $departments = $user->departments;
+        foreach ($departments as $department) {
+          if ($department->is_internal) {
+            $this->qrService->addQr($user,'/account/vcard/'.$user->id,'vcard');
+          }
+        }
+      } catch (\Exception $e) {
+        \Log::error('Iprofile: Events|Handlers|CreateQrByDepartments|Message: ' . $e->getMessage() . ' | FILE: ' . $e->getFile() . ' | LINE: ' . $e->getLine());
+      }
+    }
+  }
+}
