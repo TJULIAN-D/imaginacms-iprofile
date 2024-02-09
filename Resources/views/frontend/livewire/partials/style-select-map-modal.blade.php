7cef33a7b4c9b927aefd840ab0f59d0c293a52a7
William Verde <williamverde10@gmail.com>
2024-02-09 18:03:34 -0400

    Fix: style en select map case modal

--- Resources/views/frontend/livewire/partials/style-select-map-modal.blade.php
+++ Resources/views/frontend/livewire/partials/style-select-map-modal.blade.php
@@ -1,17 +1,17 @@
 <style>
     .pac-container {
         background-color: #FFF;
-        z-index: 20;
+        z-index: 200;
         position: fixed;
         display: inline-block;
         float: left;
     }
 
     .modal{
-        z-index: 20;   
+        z-index: 200;   
     }
 
     .modal-backdrop{
-        z-index: 10;        
+        z-index: 100;        
     }​
 </style>
