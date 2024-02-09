e9418498c58f58c3b41de8bee2e6e21ea2edeff7
William Verde <williamverde10@gmail.com>
2024-02-09 15:47:05 -0400

    Added: validation param take in address repository

--- Repositories/Eloquent/EloquentAddressRepository.php
+++ Repositories/Eloquent/EloquentAddressRepository.php
@@ -57,7 +57,7 @@ class EloquentAddressRepository extends EloquentBaseRepository implements Addres
     if (isset($params->page) && $params->page) {
       return $query->paginate($params->take);
     } else {
-      $params->take ? $query->take($params->take) : false;//Take
+      isset($params->take) && $params->take ? $query->take($params->take) : false;//Take
       return $query->get();
     }
   }
