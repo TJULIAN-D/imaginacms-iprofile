813b2da40076dc7562b0ec59c1d8688dc7c0ead7
msolanogithub <msolano@imaginacolombia.com>
2024-08-27 17:00:31 -0500

    added custom apiRoutes

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -2,111 +2,114 @@
 
 use Illuminate\Routing\Router;
 
-$router->group(['prefix' =>'/profile/v1'], function (Router $router) {
-    $router->apiCrud([
-      'module' => 'iprofile',
-      'prefix' => 'addresses',
-      'controller' => 'AddressApiController',
-      //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
-      // 'customRoutes' => [ // Include custom routes if needed
-      //  [
-      //    'method' => 'post', // get,post,put....
-      //    'path' => '/some-path', // Route Path
-      //    'uses' => 'ControllerMethodName', //Name of the controller method to use
-      //    'middleware' => [] // if not set up middleware, auth:api will be the default
-      //  ]
-      // ]
-    ]);
-    $router->apiCrud([
-      'module' => 'iprofile',
-      'prefix' => 'departments',
-      'controller' => 'DepartmentApiController',
-      //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
-      // 'customRoutes' => [ // Include custom routes if needed
-      //  [
-      //    'method' => 'post', // get,post,put....
-      //    'path' => '/some-path', // Route Path
-      //    'uses' => 'ControllerMethodName', //Name of the controller method to use
-      //    'middleware' => [] // if not set up middleware, auth:api will be the default
-      //  ]
-      // ]
-    ]);
-    $router->apiCrud([
-      'module' => 'iprofile',
-      'prefix' => 'fields',
-      'controller' => 'FieldApiController',
-      //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
-      // 'customRoutes' => [ // Include custom routes if needed
-      //  [
-      //    'method' => 'post', // get,post,put....
-      //    'path' => '/some-path', // Route Path
-      //    'uses' => 'ControllerMethodName', //Name of the controller method to use
-      //    'middleware' => [] // if not set up middleware, auth:api will be the default
-      //  ]
-      // ]
-    ]);
-    $router->apiCrud([
-      'module' => 'iprofile',
-      'prefix' => 'provider-accounts',
-      'controller' => 'ProviderAccountApiController',
-      //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
-      // 'customRoutes' => [ // Include custom routes if needed
-      //  [
-      //    'method' => 'post', // get,post,put....
-      //    'path' => '/some-path', // Route Path
-      //    'uses' => 'ControllerMethodName', //Name of the controller method to use
-      //    'middleware' => [] // if not set up middleware, auth:api will be the default
-      //  ]
-      // ]
-    ]);
-    $router->apiCrud([
-      'module' => 'iprofile',
-      'prefix' => 'roles',
-      'controller' => 'RoleApiController',
-      //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
-      // 'customRoutes' => [ // Include custom routes if needed
-      //  [
-      //    'method' => 'post', // get,post,put....
-      //    'path' => '/some-path', // Route Path
-      //    'uses' => 'ControllerMethodName', //Name of the controller method to use
-      //    'middleware' => [] // if not set up middleware, auth:api will be the default
-      //  ]
-      // ]
-    ]);
-    $router->apiCrud([
-      'module' => 'iprofile',
-      'prefix' => 'settings',
-      'controller' => 'SettingApiController',
-      //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
-      // 'customRoutes' => [ // Include custom routes if needed
-      //  [
-      //    'method' => 'post', // get,post,put....
-      //    'path' => '/some-path', // Route Path
-      //    'uses' => 'ControllerMethodName', //Name of the controller method to use
-      //    'middleware' => [] // if not set up middleware, auth:api will be the default
-      //  ]
-      // ]
-    ]);
-    $router->apiCrud([
-      'module' => 'iprofile',
-      'prefix' => 'userpasswordhistories',
-      'controller' => 'UserPasswordHistoryApiController',
-      //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
-      // 'customRoutes' => [ // Include custom routes if needed
-      //  [
-      //    'method' => 'post', // get,post,put....
-      //    'path' => '/some-path', // Route Path
-      //    'uses' => 'ControllerMethodName', //Name of the controller method to use
-      //    'middleware' => [] // if not set up middleware, auth:api will be the default
-      //  ]
-      // ]
-    ]);
+$router->group(['prefix' => '/profile/v1'], function (Router $router) {
+  //======  AUTH  <---
+  require('ApiRoutes/authRoutes.php');
+  //======  USERS
+  require('ApiRoutes/userRoutes.php');
+
+  //======  APP
+  require('ApiRoutes/appRoutes.php');
+
+  $router->apiCrud([
+    'module' => 'iprofile',
+    'prefix' => 'addresses',
+    'controller' => 'AddressApiController',
+    //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
+    // 'customRoutes' => [ // Include custom routes if needed
+    //  [
+    //    'method' => 'post', // get,post,put....
+    //    'path' => '/some-path', // Route Path
+    //    'uses' => 'ControllerMethodName', //Name of the controller method to use
+    //    'middleware' => [] // if not set up middleware, auth:api will be the default
+    //  ]
+    // ]
+  ]);
+  $router->apiCrud([
+    'module' => 'iprofile',
+    'prefix' => 'departments',
+    'controller' => 'DepartmentApiController',
+    //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
+    // 'customRoutes' => [ // Include custom routes if needed
+    //  [
+    //    'method' => 'post', // get,post,put....
+    //    'path' => '/some-path', // Route Path
+    //    'uses' => 'ControllerMethodName', //Name of the controller method to use
+    //    'middleware' => [] // if not set up middleware, auth:api will be the default
+    //  ]
+    // ]
+  ]);
+  $router->apiCrud([
+    'module' => 'iprofile',
+    'prefix' => 'fields',
+    'controller' => 'FieldApiController',
+    //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
+    // 'customRoutes' => [ // Include custom routes if needed
+    //  [
+    //    'method' => 'post', // get,post,put....
+    //    'path' => '/some-path', // Route Path
+    //    'uses' => 'ControllerMethodName', //Name of the controller method to use
+    //    'middleware' => [] // if not set up middleware, auth:api will be the default
+    //  ]
+    // ]
+  ]);
+  $router->apiCrud([
+    'module' => 'iprofile',
+    'prefix' => 'provider-accounts',
+    'controller' => 'ProviderAccountApiController',
+    //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
+    // 'customRoutes' => [ // Include custom routes if needed
+    //  [
+    //    'method' => 'post', // get,post,put....
+    //    'path' => '/some-path', // Route Path
+    //    'uses' => 'ControllerMethodName', //Name of the controller method to use
+    //    'middleware' => [] // if not set up middleware, auth:api will be the default
+    //  ]
+    // ]
+  ]);
+  $router->apiCrud([
+    'module' => 'iprofile',
+    'prefix' => 'roles',
+    'controller' => 'RoleApiController',
+    //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
+    // 'customRoutes' => [ // Include custom routes if needed
+    //  [
+    //    'method' => 'post', // get,post,put....
+    //    'path' => '/some-path', // Route Path
+    //    'uses' => 'ControllerMethodName', //Name of the controller method to use
+    //    'middleware' => [] // if not set up middleware, auth:api will be the default
+    //  ]
+    // ]
+  ]);
+  $router->apiCrud([
+    'module' => 'iprofile',
+    'prefix' => 'settings',
+    'controller' => 'SettingApiController',
+    //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
+    // 'customRoutes' => [ // Include custom routes if needed
+    //  [
+    //    'method' => 'post', // get,post,put....
+    //    'path' => '/some-path', // Route Path
+    //    'uses' => 'ControllerMethodName', //Name of the controller method to use
+    //    'middleware' => [] // if not set up middleware, auth:api will be the default
+    //  ]
+    // ]
+  ]);
+  $router->apiCrud([
+    'module' => 'iprofile',
+    'prefix' => 'userpasswordhistories',
+    'controller' => 'UserPasswordHistoryApiController',
+    //'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
+    // 'customRoutes' => [ // Include custom routes if needed
+    //  [
+    //    'method' => 'post', // get,post,put....
+    //    'path' => '/some-path', // Route Path
+    //    'uses' => 'ControllerMethodName', //Name of the controller method to use
+    //    'middleware' => [] // if not set up middleware, auth:api will be the default
+    //  ]
+    // ]
+  ]);
 // append
-
-
-
-
-
 
 
 });
