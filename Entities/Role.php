a1007aca42126063131fdf7f3802789ed20a7043
Cristian Marin <marin101998@gmail.com>
2024-10-10 15:15:37 -0500

    + fix | v10 10-10-24 rollback entity role with trait translatable and change name params in herlper

--- Entities/Role.php
+++ Entities/Role.php
@@ -6,45 +6,50 @@ use Cartalyst\Sentinel\Roles\EloquentRole;
 use Modules\Core\Icrud\Traits\hasEventsWithBindings;
 use Modules\Iforms\Support\Traits\Formeable;
 use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
+use Astrotomic\Translatable\Translatable;
 
 class Role extends EloquentRole
 {
-    use Formeable, BelongsToTenant, hasEventsWithBindings;
+  use Formeable, BelongsToTenant, hasEventsWithBindings, Translatable;
 
-    public function __construct(array $attributes = [])
-    {
+  public function __construct(array $attributes = [])
+  {
 
-        parent::__construct($attributes);
+    parent::__construct($attributes);
 
-    }
+  }
 
-    public $transformer = 'Modules\Iprofile\Transformers\RoleTransformer';
-    public $repository = 'Modules\Iprofile\Repositories\RoleApiRepository';
-    public $requestValidation = [
-        'create' => 'Modules\Iprofile\Http\Requests\CreateRoleRequest',
-        'update' => 'Modules\Iprofile\Http\Requests\UpdateRoleRequest',
-    ];
-    //Instance external/internal events to dispatch with extraData
-    public $dispatchesEventsWithBindings = [
-        //eg. ['path' => 'path/module/event', 'extraData' => [/*...optional*/]]
-        'created' => [],
-        'creating' => [],
-        'updated' => [],
-        'updating' => [],
-        'deleting' => [],
-        'deleted' => []
-    ];
+  public $transformer = 'Modules\Iprofile\Transformers\RoleTransformer';
+  public $repository = 'Modules\Iprofile\Repositories\RoleApiRepository';
+  public $requestValidation = [
+    'create' => 'Modules\Iprofile\Http\Requests\CreateRoleRequest',
+    'update' => 'Modules\Iprofile\Http\Requests\UpdateRoleRequest',
+  ];
+  //Instance external/internal events to dispatch with extraData
+  public $dispatchesEventsWithBindings = [
+    //eg. ['path' => 'path/module/event', 'extraData' => [/*...optional*/]]
+    'created' => [],
+    'creating' => [],
+    'updated' => [],
+    'updating' => [],
+    'deleting' => [],
+    'deleted' => []
+  ];
 
-    protected $fillable = [
-        'slug',
-        'name',
-        'permissions'
-    ];
+  protected $fillable = [
+    'slug',
+    'name',
+    'permissions'
+  ];
 
-    public $tenantWithCentralData = false;
+  public $translatedAttributes = [
+    'title'
+  ];
 
-    public function settings()
-    {
-        return $this->hasMany(Setting::class, 'related_id')->where('entity_name', 'role');
-    }
+  public $tenantWithCentralData = false;
+
+  public function settings()
+  {
+    return $this->hasMany(Setting::class, 'related_id')->where('entity_name', 'role');
+  }
 }
