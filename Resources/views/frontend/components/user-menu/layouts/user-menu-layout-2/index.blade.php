8bee018e34792f988e30e2e254fb771aeeab6fb4
josuebedoya <josuebedoya348@gmail.com>
2024-12-18 11:41:08 -0500

    fix: Backend | Remove $showlabel and add $typeContent

--- Resources/views/frontend/components/user-menu/layouts/user-menu-layout-2/index.blade.php
+++ Resources/views/frontend/components/user-menu/layouts/user-menu-layout-2/index.blade.php
@@ -1,4 +1,3 @@
-@livewire("iprofile::user-menu", [ "layout" => $layout, "showLabel" => $showLabel, "id" => $id,
-"params" => $params, $openLoginInModal => $openLoginInModal, "openRegisterInModal" => $openRegisterInModal,
+@livewire("iprofile::user-menu", [ "layout" => $layout, "id" => $id,"params" => $params, $openLoginInModal => $openLoginInModal, "openRegisterInModal" => $openRegisterInModal,
 "onlyShowInTheDropdownHeader" => $onlyShowInTheDropdownHeader, "onlyShowInTheMenuOfTheIndexProfilePage" => $onlyShowInTheMenuOfTheIndexProfilePage,
-"label" => $label, "classUser" => $classUser, "styleUser" => $styleUser])
+"label" => $label, "classUser" => $classUser, "styleUser" => $styleUser, "typeContent" => $typeContent])
