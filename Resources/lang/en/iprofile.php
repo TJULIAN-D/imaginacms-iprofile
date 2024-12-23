b2c1ac836ccfd27f9022d2a0a5198ba6bc7cdb8e
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2024-12-23 18:21:25 -0500

    Fixed: translation about messageChangePassword

--- Resources/lang/en/iprofile.php
+++ Resources/lang/en/iprofile.php
@@ -4,7 +4,7 @@ return [
   'name' => 'Users',
   'notifications' => [
     'titleChangePassword' => 'Set your password to access your account',
-    'messageChangePassword' => 'To ensure the security of your account, please set your password by following the link below: <a href=":linkPassword">:linkPassword</a>. This link will be valid for a limited time. If you encounter any issues or need assistance, feel free to contact us.'
+    'messageChangePassword' => 'To ensure the security of your account, please set your password by following the link below: <a href=":linkPassword">Click Here</a>. This link will be valid for a limited time. If you encounter any issues or need assistance, feel free to contact us.'
   ]
 
 ];
--- Resources/lang/es/iprofile.php
+++ Resources/lang/es/iprofile.php
@@ -5,7 +5,7 @@ return [
   'notifications' => [
     'titleChangePassword' => 'Configura tu contraseña para acceder a tu cuenta',
     'messageChangePassword' => 'Para asegurarte de que tu cuenta esté segura, por favor, asigna tu contraseña siguiendo 
-                                el enlace a continuación: <a href=":linkPassword">:linkPassword</a> Este enlace será 
+                                el enlace a continuación: <a href=":linkPassword">Click Aqui</a> Este enlace será 
                                 válido por un tiempo limitado. Si tienes algún problema o necesitas ayuda, no dudes en 
                                 contactarnos.'
   ]
