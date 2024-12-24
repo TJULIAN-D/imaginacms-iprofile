4d4193a21cf9362892bd47b6230c36e8cbe49d41
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2024-12-23 22:21:57 -0500

    Fixed: Improve email message

--- Resources/lang/es/iprofile.php
+++ Resources/lang/es/iprofile.php
@@ -4,9 +4,12 @@ return [
   'name' => 'Usuarios',
   'notifications' => [
     'titleChangePassword' => 'Configura tu contraseña para acceder a tu cuenta',
-    'messageChangePassword' => 'Para asegurarte de que tu cuenta esté segura, por favor, asigna tu contraseña siguiendo 
-                                el enlace a continuación: <a href=":linkPassword">Click Aqui</a> Este enlace será 
-                                válido por un tiempo limitado. Si tienes algún problema o necesitas ayuda, no dudes en 
-                                contactarnos.'
+    'messageChangePassword' => '<p>Para asegurarte de que tu cuenta esté segura, por favor asigna tu contraseña siguiendo el enlace a continuación:</p>
+    <p style="text-align: center; margin: 20px 0;">
+        <a href=":linkPassword" style="display: inline-block; background-color: #007BFF; color: #fff; text-decoration: none; padding: 10px 20px; border-radius: 5px; font-weight: bold;">
+            Click Aquí
+        </a>
+    </p>
+    <p>Este enlace será válido por un tiempo limitado. Si tienes algún problema o necesitas ayuda, no dudes en contactarnos.</p>'
   ]
 ];
