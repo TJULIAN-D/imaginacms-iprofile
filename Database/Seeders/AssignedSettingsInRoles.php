fdb6aeec82427ff4de68ecec1e8cf491b842d92b
Cristian Marin <marin101998@gmail.com>
2025-02-27 11:42:12 -0500

    + fix | v10 27-02-25 update assigned settings only role admin

--- Database/Seeders/AssignedSettingsInRoles.php
+++ Database/Seeders/AssignedSettingsInRoles.php
@@ -8,51 +8,53 @@ use Modules\Iprofile\Entities\Setting;
 
 class AssignedSettingsInRoles extends Seeder
 {
-    public function run()
-    {
-        Model::unguard();
+  public function run()
+  {
+    Model::unguard();
 
-        $module = app('modules');
-        $rolesRepository = app("Modules\Iprofile\Repositories\RoleApiRepository");
-        $settingsRepository = app("Modules\Setting\Repositories\SettingRepository");
-        $roles = $rolesRepository->getItemsBy();
-        $data = [];
-        $translatableSettings = [];
-        $plainSettings = [];
 
-        $modulesWithSettings = $settingsRepository->moduleSettings($module->allEnabled());
+    $module = app('modules');
+    $rolesRepository = app("Modules\Iprofile\Repositories\RoleApiRepository");
+    $settingsRepository = app("Modules\Setting\Repositories\SettingRepository");
+    $params = [];
+    $roles = $rolesRepository->getItemsBy($params);
+    $data = [];
+    $translatableSettings = [];
+    $plainSettings = [];
 
-        foreach ($modulesWithSettings as $key => $module) {
-            $translatableSettings[$key] = $settingsRepository->translatableModuleSettings($key);
-            $plainSettings[$key] = $settingsRepository->plainModuleSettings($key);
-        }
+    $modulesWithSettings = $settingsRepository->moduleSettings($module->allEnabled());
 
-        $mergedSettings = array_merge_recursive($translatableSettings, $plainSettings);
-
-        $modules = json_decode(json_encode($mergedSettings));
-
-        foreach ($modules as $key => $module) {
-            $moduleName = strtolower($key);
-            foreach ($module as $key => $setting) {
-                if (isset($setting) && isset($setting->onlySuperAdmin) && $setting->onlySuperAdmin == false) {
-                    $settingAssigned = $moduleName.'::'.$key;
-                    array_push($data, $settingAssigned);
-                }
-            }
-        }
-        foreach ($roles as $role) {
-            if (isset($role->slug) && $role->slug != 'super-admin') {
-                // Update or create the setting
-                Setting::updateOrCreate(
-                    ['related_id' => $role->id, 'entity_name' => 'role', 'name' => 'assignedSettings'],
-                    [
-                        'related_id' => $role->id,
-                        'entity_name' => 'role',
-                        'name' => 'assignedSettings',
-                        'value' => $data,
-                    ]
-                );
-            }
-        }
+    foreach ($modulesWithSettings as $key => $module) {
+      $translatableSettings[$key] = $settingsRepository->translatableModuleSettings($key);
+      $plainSettings[$key] = $settingsRepository->plainModuleSettings($key);
     }
+
+    $mergedSettings = array_merge_recursive($translatableSettings, $plainSettings);
+
+    $modules = json_decode(json_encode($mergedSettings));
+
+    foreach ($modules as $key => $module) {
+      $moduleName = strtolower($key);
+      foreach ($module as $key => $setting) {
+        if (isset($setting) && isset($setting->onlySuperAdmin) && $setting->onlySuperAdmin == false) {
+          $settingAssigned = $moduleName . '::' . $key;
+          array_push($data, $settingAssigned);
+        }
+      }
+    }
+    foreach ($roles as $role) {
+      if (isset($role->slug) && $role->slug == 'admin') {
+        // Update or create the setting
+        Setting::updateOrCreate(
+          ['related_id' => $role->id, 'entity_name' => 'role', 'name' => 'assignedSettings'],
+          [
+            'related_id' => $role->id,
+            'entity_name' => 'role',
+            'name' => 'assignedSettings',
+            'value' => $data,
+          ]
+        );
+      }
+    }
+  }
 }
--- Database/Seeders/IprofileDatabaseSeeder.php
+++ Database/Seeders/IprofileDatabaseSeeder.php
@@ -6,18 +6,18 @@ use Illuminate\Database\Seeder;
 
 class IprofileDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run()
-    {
-        $this->call(IprofileModuleTableSeeder::class);
-        $this->call(DepartmentTableSeeder::class);
-        $this->call(UserDepartmentTableSeeder::class);
-        $this->call(RolePermissionsSeeder::class);
-        $this->call(RolePermissionsToAccessSeeder::class);
-        $this->call(IformUserDefaultRegisterTableSeeder::class);
-        $this->call(LayoutsProfileTableSeeder::class);
-//        $this->call(AssignedSettingsInRoles::class);
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run()
+  {
+    $this->call(IprofileModuleTableSeeder::class);
+    $this->call(DepartmentTableSeeder::class);
+    $this->call(UserDepartmentTableSeeder::class);
+    $this->call(RolePermissionsSeeder::class);
+    $this->call(RolePermissionsToAccessSeeder::class);
+    $this->call(IformUserDefaultRegisterTableSeeder::class);
+    $this->call(LayoutsProfileTableSeeder::class);
+    $this->call(AssignedSettingsInRoles::class);
+  }
 }
