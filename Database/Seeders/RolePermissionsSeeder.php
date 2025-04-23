d4523b38de9db5d211e40363f88493ff8c49c1d8
Cristian Marin <marin101998@gmail.com>
2025-04-23 16:49:25 -0500

    + fix | v10 23-04-25 Fix seed role permissions seeder

--- Database/Seeders/RolePermissionsSeeder.php
+++ Database/Seeders/RolePermissionsSeeder.php
@@ -13,87 +13,87 @@ use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 
 class RolePermissionsSeeder extends Seeder
 {
-    private $permissions;
+  private $permissions;
   private $profileRoleRepository;
 
-    public function __construct(PermissionManager $permissions)
-    {
-        $this->permissions = $permissions;
+  public function __construct(PermissionManager $permissions)
+  {
+    $this->permissions = $permissions;
     $this->profileRoleRepository = app("Modules\Iprofile\Repositories\RoleApiRepository");
-    }
+  }
 
-    /**
-     * Run the database seeds.
-     */
-    public function run()
-    {
-        Model::unguard();
+  /**
+   * Run the database seeds.
+   */
+  public function run()
+  {
+    Model::unguard();
 
     //Added because when the job was called again in the creation of the tenant, it did not find the admin role
-    if(isset(tenant()->id))
+    if (isset(tenant()->id))
       forceInitializeTenant(tenant()->id);
 
-        $roles = Sentinel::getRoleRepository();
-    
-    $params = ["filter" => ["field" => "slug"],"include" => [],"fields" => []];
+    $roles = Sentinel::getRoleRepository();
+
+    $params = ["filter" => ["field" => "slug"], "include" => [], "fields" => []];
     $admin = $this->profileRoleRepository->getItem("admin", json_decode(json_encode($params)));
 
-        if (! isset($admin->id)) {
-      
+    if (!isset($admin->id)) {
+
       $permissions = $this->permissions->all();
-  
+
       $this->module = app('modules');
       $modules = array_keys($this->module->allEnabled());
-  
+
       $allPermissions = [];
-  
+
       //Get permissions and set true
       foreach ($permissions as $moduleName => $modulePermissions) {
-        if(isset($modules[$moduleName]))
+        if (in_array($moduleName, $modules))
           foreach ($modulePermissions as $entityName => $entityPermissions) {
             foreach ($entityPermissions as $permissionName => $permission) {
               $allPermissions["{$entityName}.{$permissionName}"] = true;
             }
           }
       }
-      
+
     }
 
     // Create or Update data to Role
     $roleData = [
-                    'name' => 'Admin',
-                    'slug' => 'admin',
-      'en' => ['title' => trans("iprofile::roles.types.admin",[],"en")],
-      'es' => ['title' => trans("iprofile::roles.types.admin",[],"es")]
+      'name' => 'Admin',
+      'slug' => 'admin',
+      'en' => ['title' => trans("iprofile::roles.types.admin", [], "en")],
+      'es' => ['title' => trans("iprofile::roles.types.admin", [], "es")]
     ];
 
     //Only when create
-    if(isset($allPermissions))
+    if (isset($allPermissions))
       $roleData['permissions'] = $allPermissions;
 
     $admin = createOrUpdateRole($roleData);
 
-        // Find all other Roles to assign it
-        $allOtherRoles = Role::where('slug', '!=', 'super-admin')->get();
+    // Find all other Roles to assign it
+    $allOtherRoles = Role::where('slug', '!=', 'super-admin')->get();
 
-        // Create default Setting to the admin (assignedRoles,assignedSettings)
-        $adminAssignedRoles = Setting::where('related_id', $admin->id)
-          ->where('entity_name', 'role')
-          ->where('name', 'assignedRoles')
-          ->first();
+    // Create default Setting to the admin (assignedRoles,assignedSettings)
+    $adminAssignedRoles = Setting::where('related_id', $admin->id)
+      ->where('entity_name', 'role')
+      ->where('name', 'assignedRoles')
+      ->first();
 
-        if (! isset($adminAssignedRoles->id)) {
-            Setting::create(
-                [
-                    'related_id' => $admin->id,
-                    'entity_name' => 'role',
-                    'name' => 'assignedRoles',
+    if (!isset($adminAssignedRoles->id)) {
+      Setting::create(
+        [
+          'related_id' => $admin->id,
+          'entity_name' => 'role',
+          'name' => 'assignedRoles',
           'value' => $allOtherRoles->pluck('id')->toArray()
         ]);
-    }else{
+    } else {
       $adminAssignedRoles->update([
         "value" => $allOtherRoles->pluck('id')->toArray()
-                ]);
-        }
+      ]);
     }
+  }
 }
